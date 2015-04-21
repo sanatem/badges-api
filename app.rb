828c682e8b74540a0b5aaa4020fa84a35d02f8c7
Santiago Pravisani <santiagopravisani@gmail.com>
2015-04-21 15:39:44 -0300

    Primeras rutas agregadas.

--- app.rb
+++ app.rb
@@ -51,6 +51,42 @@ end
     JSON.pretty_generate({'Welcome to:'=>'Badges Api'})
   end	
 
+  get '/badges' do
+    slug="90812gjd"
+    name="Joven Cientifico"
+    [{id_badge_class:slug,name:name},{id_badge_class:slug,name:"Cientifico Avanzado"}].to_json
+
+  end
+
+  get '/issuers/:id_app/badges' do
+    
+    status 200
+
+    [
+      {
+          id_badge_class: "h1k2bfhg",
+          name: "Asesino de aliens"
+      },
+      {
+          id_badge_class: "2gd1d2w3g",
+          name: "Sherlock Holmes Espacial"
+      },
+      {
+          id_badge_class: "2134gdsa8",
+          name: "Stephen Hawking"
+      }
+    ].to_json
+  end
+
+  post '/badges/:id_badge_class/instances' do
+
+    json_status 200, "ok"
+
+  end
+
+  get '/error' do
+    json_status 404,"Not found"
+  end
 
 
 end
